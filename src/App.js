e9902f5ee03c9ffd0c025de7191e541bba3333eb
ashu-saini-bs <ashu.saini@bigsteptech.com>
2020-09-25 13:06:49 +0530

    feat(add jsconfig.json) : add absolute import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react"
-import Player from "./components/Player"
+import React from "react";
+import Player from "components/Player";
 
 function App() {
   const stream = {
     stream: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-  }
+  };
   return (
     <div className="App">
       <Player {...stream} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,25 +1,25 @@
-import * as React from "react"
-import Hls from "hls.js"
-import HlsJs from "./HlsJs"
-import HlsNative from "./HlsNative"
+import * as React from "react";
+import Hls from "hls.js";
+import HlsJs from "components/Player/HlsJs";
+import HlsNative from "components/Player/HlsNative";
 
 const hasNativeHlsSupport = () => {
-  const videoElem = document.createElement("video")
-  return videoElem.canPlayType("application/vnd.apple.mpegurl") !== ""
-}
+  const videoElem = document.createElement("video");
+  return videoElem.canPlayType("application/vnd.apple.mpegurl") !== "";
+};
 
 const Player = (props) => {
-  const isHlsSupported = Hls.isSupported()
+  const isHlsSupported = Hls.isSupported();
   if (props.stream) {
     if (isHlsSupported) {
-      return <HlsJs {...props} />
+      return <HlsJs {...props} />;
     } else if (hasNativeHlsSupport()) {
-      return <HlsNative {...props} />
+      return <HlsNative {...props} />;
     } else {
-      return <div>Hls Player not supported on platform.</div>
+      return <div>Hls Player not supported on platform.</div>;
     }
   }
-  return <div></div>
-}
+  return <div></div>;
+};
 
-export default Player
+export default Player;
